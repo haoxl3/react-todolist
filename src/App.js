181e372c55c43e4dc9071d086232281a6c31d913
haoxl <1098262516@qq.com>
2018-11-18 23:17:10 +0800

    '动画'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import {CSSTransition} from 'react-transition-group';
 import './App.css'
 
 class App extends Component {
@@ -12,7 +13,17 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <div className={this.state.show ? 'show':'hide'}>hello</div>
+        {/*<div className={this.state.show ? 'show':'hide'}>hello</div>*/}
+        <CSSTransition
+          in={this.state.show}
+          timeout={1000}
+          classNames='fade'
+          unmountOnExit
+          onEntered={(el)=>{el.style.color='blue'}}
+          appear={true}
+        >
+          <div>hello</div>
+        </CSSTransition>
         <button onClick={this.handleToggle}>toggle</button>
       </Fragment>
     );
