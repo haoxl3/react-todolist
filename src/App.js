e34d5f0c9b11e8a6a51831a70b8169fe17337a4d
haoxl <1098262516@qq.com>
2018-11-18 20:55:54 +0800

    '动画'

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true
+    }
+    this.handleToggle = this.handleToggle.bind(this)
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <div className={this.state.show ? 'show':'hide'}>hello</div>
+        <button onClick={this.handleToggle}>toggle</button>
+      </Fragment>
     );
+  }
+  handleToggle() {
+    this.setState({
+      show: !this.state.show
+    })
   }
 }
 
